791979be26db28b3ad295e4e0b86011f7c6b67ae
shaxruxtleubaev <tleubaevshaxrux@outlook.com>
2023-09-14 21:06:28 +0500

    change clients model

--- clients/admin.py
+++ clients/admin.py
@@ -4,6 +4,6 @@ from .models import Client
 @register(Client)
 class ClientAdmin(ModelAdmin):
 
-    list_display = ('id', 'fullname', 'phone_number')
-    list_display_links = ('id', 'fullname', 'phone_number')
+    list_display = ('id', 'fullname', 'phone_number', 'tour_name')
+    list_display_links = ('id', 'fullname', 'phone_number', 'tour_name')
     ordering = ('-id',)
